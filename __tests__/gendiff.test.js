d8cfbea773f73da637b553d6f160f259a797207c
ravbetsky <ravbetsky@gmail.com>
2018-04-05 04:03:00 +0400

    update tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,16 +1,15 @@
-import genDiff from '../src';
+import path from 'path';
+import genDiff, { toString as diffToString } from '../src';
 
 test('JSON difference', () => {
-  const pathToBefore = __dirname + '/__fixtures__/before.json';
-  const pathToAfter = __dirname + '/__fixtures__/after.json';
-  const expected = `
-    {
-        host: hexlet.io
-      + timeout: 20
-      - timeout: 50
-      - proxy: 123.234.53.22
-      + verbose: true
-    }
-  `;
-  expect(genDiff(pathToBefore, pathToAfter)).toBe(expected);
+  const pathToBefore = path.join(__dirname, '/__fixtures__/before.json');
+  const pathToAfter = path.join(__dirname, '/__fixtures__/after.json');
+  const expected = [
+    { key: 'host', value: 'hexlet.io', type: 'unchanged' },
+    { key: 'timeout', value: 50, type: 'deleted' },
+    { key: 'timeout', value: 20, type: 'added' },
+    { key: 'proxy', value: '123.234.53.22', type: 'deleted' },
+    { key: 'verbose', value: true, type: 'added' },
+  ];
+  expect(genDiff(pathToBefore, pathToAfter)).toBe(diffToString(expected));
 });
