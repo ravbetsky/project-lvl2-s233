45c8683942205ef6c18c135f6211f529d0249a52
ravbetsky <ravbetsky@gmail.com>
2018-04-05 04:03:22 +0400

    add gendiff JSON algorithm

--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
 import fs from 'fs';
+import { has } from 'lodash';
 
-const genDiff = (pathToBefore, pathToAfter) => {
-  const before = JSON.parse(fs.readFileSync(pathToBefore, 'utf8'));
-  const after = JSON.parse(fs.readFileSync(pathToAfter, 'utf8'));
-  return 5;
+const symbols = new Map([['unchanged', ''], ['deleted', '-'], ['added', '+']]);
+
+export const toString = (diff) => {
+  const result = diff.reduce((acc, obj) => {
+    const { key, value, type } = obj;
+    return `${acc}  ${symbols.get(type)} ${key}: ${value}\n`;
+  }, '\n');
+  return `{${result}}`;
 };
 
-export default genDiff;
+export default (pathToBefore, pathToAfter) => {
+  const before = JSON.parse(fs.readFileSync(pathToBefore, 'utf8'));
+  const after = JSON.parse(fs.readFileSync(pathToAfter, 'utf8'));
+  const allKeys = [...new Set(Object.keys(before).concat(Object.keys(after)))];
+  const result = allKeys.reduce((acc, key) => {
+    if (has(before, key)) {
+      const oldData = { key, value: before[key], type: 'unchanged' };
+      if (has(after, key)) {
+        if (after[key] !== oldData.value) {
+          return acc.concat({ ...oldData, type: 'deleted' }, { key, value: after[key], type: 'added' });
+        }
+        return acc.concat(oldData);
+      }
+      return acc.concat({ ...oldData, type: 'deleted' });
+    }
+    return acc.concat({ key, value: after[key], type: 'added' });
+  }, []);
+  return toString(result);
+};
