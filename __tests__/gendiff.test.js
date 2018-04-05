8f06dc6f095be4c244ba9cb6ea780a756295918a
ravbetsky <ravbetsky@gmail.com>
2018-04-05 18:05:29 +0400

    fix JSON difference test

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,15 +1,11 @@
 import path from 'path';
-import genDiff, { toString as diffToString } from '../src';
+import fs from 'fs';
+import genDiff from '../src';
 
 test('JSON difference', () => {
   const pathToBefore = path.join(__dirname, '/__fixtures__/before.json');
   const pathToAfter = path.join(__dirname, '/__fixtures__/after.json');
-  const expected = [
-    { key: 'host', value: 'hexlet.io', type: 'unchanged' },
-    { key: 'timeout', value: 50, type: 'deleted' },
-    { key: 'timeout', value: 20, type: 'added' },
-    { key: 'proxy', value: '123.234.53.22', type: 'deleted' },
-    { key: 'verbose', value: true, type: 'added' },
-  ];
-  expect(genDiff(pathToBefore, pathToAfter)).toBe(diffToString(expected));
+  const pathToDiff = path.join(__dirname, '/__fixtures__/diff.txt');
+  const expected  = fs.readFileSync(pathToDiff, 'utf-8');
+  expect(genDiff(pathToBefore, pathToAfter)).toBe(expected);
 });
