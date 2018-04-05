1749cf06d84bee3df6880727dc84fbdaaefc0923
ravbetsky <ravbetsky@gmail.com>
2018-04-05 18:06:48 +0400

    fix lint errors

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -6,6 +6,6 @@ test('JSON difference', () => {
   const pathToBefore = path.join(__dirname, '/__fixtures__/before.json');
   const pathToAfter = path.join(__dirname, '/__fixtures__/after.json');
   const pathToDiff = path.join(__dirname, '/__fixtures__/diff.txt');
-  const expected  = fs.readFileSync(pathToDiff, 'utf-8');
+  const expected = fs.readFileSync(pathToDiff, 'utf-8');
   expect(genDiff(pathToBefore, pathToAfter)).toBe(expected);
 });
