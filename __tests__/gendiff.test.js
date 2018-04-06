e184fad3939812f4874cc8dfc11de714b1226a1b
ravbetsky <ravbetsky@gmail.com>
2018-04-06 11:43:23 +0400

    add YAML test

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -2,10 +2,17 @@ import path from 'path';
 import fs from 'fs';
 import genDiff from '../src';
 
+const pathToDiff = path.join(__dirname, '/__fixtures__/diff.txt');
+const expected = fs.readFileSync(pathToDiff, 'utf-8');
+
 test('JSON difference', () => {
   const pathToBefore = path.join(__dirname, '/__fixtures__/before.json');
   const pathToAfter = path.join(__dirname, '/__fixtures__/after.json');
-  const pathToDiff = path.join(__dirname, '/__fixtures__/diff.txt');
-  const expected = fs.readFileSync(pathToDiff, 'utf-8');
+  expect(genDiff(pathToBefore, pathToAfter)).toBe(expected);
+});
+
+test('YAML difference', () => {
+  const pathToBefore = path.join(__dirname, '/__fixtures__/before.yml');
+  const pathToAfter = path.join(__dirname, '/__fixtures__/after.yml');
   expect(genDiff(pathToBefore, pathToAfter)).toBe(expected);
 });
