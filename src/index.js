8b94d9a0c5a49b3441c2cf88cce7217f8589b80f
ravbetsky <ravbetsky@gmail.com>
2018-04-08 19:57:02 +0400

    fix lint errors

--- src/index.js
+++ src/index.js
@@ -4,31 +4,30 @@ import { has, union, keys } from 'lodash';
 import getParser from './parsers';
 
 const getPadding = (level, mode = ' ') =>
-  ' '.repeat((level - 1) * 4) + ' '.repeat(2) + mode + ' ';
-  
+  `${' '.repeat((level - 1) * 4)}${' '.repeat(2)}${mode} `;
+
 const stringify = (body, level) => {
   if (body instanceof Object) {
     return JSON.stringify(body, null, '\t')
       .replace(/\t+/g, getPadding(level + 1))
-      .replace(/\"/g, '')
-      .replace(/\}/g, `${getPadding(level)}}`)
+      .replace(/"/g, '')
+      .replace(/\}/g, `${getPadding(level)}}`);
   }
   return body;
-}
+};
 
 const symbols = new Map([['unchanged', ' '], ['deleted', '-'], ['added', '+']]);
 
-const node = (key) => (body, type = 'unchanged') => {
-  return { key, ...body, type };
-}
+const node = key => (body, type = 'unchanged') => ({ key, ...body, type });
 
 const render = (tree, level = 0) => {
-  const { key, children, value, type } = tree;
-  const body = children ? '{\n' + children.map((child) => render(child, level + 1)).join('\n') + `\n${' '.repeat((level) * 4)}}` : value;
+  const { key, type, ...body } = tree;
+  const { value, children } = body;
+  const result = children ? `{\n${children.map(el => render(el, level + 1)).join('\n')}\n${' '.repeat((level) * 4)}}` : value;
   if (key) {
-    return `${getPadding(level, symbols.get(type))}${key}: ${stringify(body, level)}`
+    return `${getPadding(level, symbols.get(type))}${key}: ${stringify(result, level)}`;
   }
-  return `${body}`;
+  return `${result}`;
 };
 
 const build = (before, after) => {
@@ -49,7 +48,7 @@ const build = (before, after) => {
     return acc.concat(makeNode({ value: after[key] }, 'added'));
   }, []);
   return result;
-}
+};
 
 export default (pathToBefore, pathToAfter) => {
   const extBefore = path.extname(pathToBefore);
@@ -58,4 +57,4 @@ export default (pathToBefore, pathToAfter) => {
   const after = getParser(extAfter)(fs.readFileSync(pathToAfter, 'utf8'));
   const result = { children: build(before, after) };
   return render(result);
-}
+};
