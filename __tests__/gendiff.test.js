539d62d4edf3c30b9a25247035fb5058df23d144
ravbetsky <ravbetsky@gmail.com>
2018-04-08 19:43:04 +0400

    add more complex tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -13,16 +13,16 @@ test('JSON difference', () => {
 });
 
 test('YAML difference', () => {
-  const pathToBefore = getFixturePath('before.json');
-  const pathToAfter = getFixturePath('after.json');
+  const pathToBefore = getFixturePath('before.yml');
+  const pathToAfter = getFixturePath('after.yml');
   const pathToDiff = getFixturePath('diff.txt');
   const expected = fs.readFileSync(pathToDiff, 'utf-8');
   expect(genDiff(pathToBefore, pathToAfter)).toBe(expected);
 });
 
 test('INI difference', () => {
-  const pathToBefore = getFixturePath('before.json');
-  const pathToAfter = getFixturePath('after.json');
+  const pathToBefore = getFixturePath('before.ini');
+  const pathToAfter = getFixturePath('after.ini');
   const pathToDiff = getFixturePath('diff.txt');
   const expected = fs.readFileSync(pathToDiff, 'utf-8');
   expect(genDiff(pathToBefore, pathToAfter)).toBe(expected);
